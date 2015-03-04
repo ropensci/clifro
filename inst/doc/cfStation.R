f574e96776d0a206f18327542b7c8a3934728cd9
blasee <blake.seers@gmail.com>
2015-03-04 23:00:21 +1300

    update vignettes so they don't rely on connection to CliFlo

--- inst/doc/cfStation.R
+++ inst/doc/cfStation.R
@@ -1,17 +1,21 @@
-## ----, echo=FALSE--------------------------------------------------------
+## ----, echo = FALSE------------------------------------------------------
 library(clifro)
 
-## ------------------------------------------------------------------------
-lake.tekapo.st = cf_station(12709, 35567, 39557, 4630, 24945, 4616, 4602)
-lake.tekapo.st[, c("name", "agent", "start", "end", "open")]
+## ----, eval = FALSE------------------------------------------------------
+#  lake.tekapo.st = cf_station(12709, 35567, 39557, 4630, 24945, 4616, 4602)
+#  lake.tekapo.st[, c("name", "agent", "start", "end", "open")]
 
-## ------------------------------------------------------------------------
-added.stations.st = lake.tekapo.st + 
-  cf_station() + 
-  cf_find_station("lighthouse", status = "all")
-added.stations.st[, c("name", "agent", "start", "end", "open")]
+## ----, eval = FALSE------------------------------------------------------
+#  added.stations.st = lake.tekapo.st +
+#    cf_station() +
+#    cf_find_station("lighthouse", status = "all")
+#  added.stations.st[, c("name", "agent", "start", "end", "open")]
 
-## ----,message=FALSE,fig.width=7,fig.height=7, dpi=50, cache=TRUE---------
+## ----, echo = FALSE------------------------------------------------------
+auckland.df = dget("auckStations")
+auckland.st = new("cfStation", auckland.df)
+
+## ----, eval = -c(2:3), message = FALSE-----------------------------------
 library(ggmap)
 
 # Conduct the search
