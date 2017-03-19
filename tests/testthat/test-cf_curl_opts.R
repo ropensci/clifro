48953af2a4fbcd00057545a807c1493d169ec6b8
steven2249 <stevenysw@berkeley.edu>
2017-03-19 10:57:11 -0700

    Update test-cf_curl_opts.R

--- tests/testthat/test-cf_curl_opts.R
+++ tests/testthat/test-cf_curl_opts.R
@@ -4,4 +4,7 @@ test_that("cf_curl_opts", {
   tt <- cf_curl_opts()
   
   expect_is(tt, "list")
+  expect_is(tt$followlocation,"logical")
+  expect_is(tt$useragent, "character")
+  expect_is(tt$timeout, "numeric")
 })
